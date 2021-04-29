c2ce5354c64724504ee92143dfcf2809ccd1e951
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2021-04-29 23:39:56 +0600

    button list show

--- app/src/main/java/com/example/oceanizeapplication/MainActivity.kt
+++ app/src/main/java/com/example/oceanizeapplication/MainActivity.kt
@@ -1,32 +1,52 @@
 package com.example.oceanizeapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import MyUtil
 import android.os.Bundle
 import android.util.Log
-import androidx.core.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cn.pedant.SweetAlert.SweetAlertDialog
+import com.example.oceanizeapplication.model.DataModelResponse
 import com.example.oceanizeapplication.view_model.DataViewModel
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataViewModel: DataViewModel
+    private var recyclerView: RecyclerView? = null
+    private var mAdapter: RequestAdapter? = null
+    private lateinit var pDialog: SweetAlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        pDialog = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
+        pDialog.titleText = "Loading"
+        pDialog.setCancelable(false)
+        recyclerView = findViewById(R.id.rv_users) as RecyclerView
         dataViewModel = ViewModelProvider(this).get(DataViewModel::class.java)
         observeViewModel()
         getResponse()
+        val mLayoutManager = LinearLayoutManager(applicationContext)
+        recyclerView!!.setLayoutManager(mLayoutManager)
+        recyclerView!!.setItemAnimator(DefaultItemAnimator())
+
     }
     fun observeViewModel() {
         dataViewModel.response_error .observe(this, androidx.lifecycle.Observer {
             it?.let {
-                if(it){
-                    Log.e("ButtonList","-------------Error")
-                }else{
-                    Log.e("ButtonList","-------------No Error")
-                }
+                pDialog.dismiss()
+                SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Warning")
+                    .setContentText("Can not fetch")
+                    .show()
 
             }
         })
@@ -34,19 +54,61 @@ class MainActivity : AppCompatActivity() {
         dataViewModel.listResponse .observe(this, androidx.lifecycle.Observer {
 
             it?.let {
-
+                pDialog.dismiss()
                 for (i in 0 until it.size) {
                     Log.e("ButtonList",it.get(i).name.toString())
                 }
+                Toast.makeText(getApplication(), "Fetch From Database ", Toast.LENGTH_SHORT).show()
+                mAdapter = RequestAdapter(it)
+                recyclerView!!.setAdapter(mAdapter)
+                mAdapter?.notifyDataSetChanged();
+
             }
-
-
         })
-
     }
     private fun getResponse() {
 
+        if(MyUtil.isOnline(this)){
+            pDialog.show()
+            this.let { it1 -> dataViewModel.responseList() }
+        }else{
+            SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("Warning")
+                .setContentText("Need Internet Connection")
+                .show()
+        }
 
-        this.let { it1 -> dataViewModel.responseList() }
+    }
+    inner class RequestAdapter(private val requestCustomersList: List<DataModelResponse>) :
+        RecyclerView.Adapter<RequestAdapter.MyViewHolder>(), View.OnClickListener {
+
+        override fun onClick(v: View) {
+
+        }
+
+        inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            var btnCommand: Button
+
+            init {
+                btnCommand = view.findViewById(R.id.btnCommand) as Button
+
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_layout, parent, false)
+
+            return MyViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val requestMoneyModel = requestCustomersList[position]
+            holder.btnCommand.setText(requestMoneyModel.name)
+        }
+
+        override fun getItemCount(): Int {
+            return requestCustomersList.size
+        }
     }
 }
---
+++ app/src/main/java/com/example/oceanizeapplication/util/MyUtil.kt
@@ -0,0 +1,15 @@
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+
+public class MyUtil {
+    companion object {
+
+        public fun isOnline(activity: Activity): Boolean {
+            val cm = activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val netInfo = cm.activeNetworkInfo
+            return netInfo != null && netInfo.isConnected
+        }
+    }
+
+}
